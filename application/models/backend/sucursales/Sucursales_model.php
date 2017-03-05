daa65bd6af50252baec61165f6c6c5e62b749f0d
rafael.gutierrez <rgutierrezt@gmail.com>
2017-03-05 01:37:52 -0600

    pos pedidos-detalle

--- application/models/backend/sucursales/Sucursales_model.php
+++ application/models/backend/sucursales/Sucursales_model.php
@@ -18,7 +18,15 @@ class sucursales_model extends CI_Model
     const sucursal_suarios = 'sys_sucursal_int_usuarios';
     const producto_detalle = 'sys_detalle_producto';
     const unidad_medida = 'sys_unidad_medida';
+    const inventario_sucursal = 'sys_catalogo_inventario_sucursal';
+    const catalogo_materiales = 'sys_catalogo_materiales'; 
+    const unidades_equivalentes = 'sys_unidades_equivalentes'; 
+    const sys_pedido = 'sys_pedido'; 
+    const sys_pedido_detalle = 'sys_pedido_detalle'; 
+    const sys_pedido_detalle_materia = 'sys_pedido_detalle_materia'; 
+
     
+
     
 
     
@@ -185,15 +193,20 @@ class sucursales_model extends CI_Model
 
     // Validacion de Materias en exsitencia por producto
     public function getProductoItems($sucursal,$id_producto){
-        $this->db->select('*');
+        $this->db->select('P.nombre_producto,PS.id_producto,S.nombre_sucursal,PD.name_detalle,PD.cantidad,PD.unidad_medida_id,
+            UM.nombre_unidad_medida,UM.simbolo_unidad_medida,IS.total_existencia,CM.id_unidad_medida,SUM.nombre_unidad_medida AS NombreUnidad2,SUM.simbolo_unidad_medida AS Simbolo2,SUM.id_unidad_medida AS Unidad2');
         $this->db->from(self::sys_productos.' AS P');
         $this->db->join(self::sys_productos_sucursal.' AS PS',' on P.id_producto = '.'PS.id_producto');
         $this->db->join(self::sys_sucursal.' AS S',' on S.id_sucursal = '.'PS.id_sucursal');
         $this->db->join(self::producto_detalle.' AS PD',' on PD.id_producto = '.'P.id_producto');
-        $this->db->join(self::unidad_medida.' AS UD',' on UD.id_unidad_medida = '.'PD.unidad_medida_id');
+        $this->db->join(self::unidad_medida.' AS UM',' on UM.id_unidad_medida = '.'PD.unidad_medida_id');        
+        $this->db->join(self::inventario_sucursal.' AS IS',' on IS.codigo_meterial = '.'PD.name_detalle');
+        $this->db->join(self::catalogo_materiales.' AS CM',' on CM.codigo_material = '.'IS.codigo_meterial');
+        $this->db->join(self::unidad_medida.' AS SUM',' on SUM.id_unidad_medida = '.'CM.id_unidad_medida');
         $this->db->where('S.id_sucursal',$sucursal);
         $this->db->where('P.id_producto',$id_producto);
         $query = $this->db->get();
+        //echo $this->db->queries[0];
         
         if($query->num_rows() > 0 )
         {
@@ -201,7 +214,71 @@ class sucursales_model extends CI_Model
         } 
     }
 
+    public function getValidarDescuentoInventario($codigo_material){
+        $this->db->select('*');
+        $this->db->from(self::catalogo_materiales.' AS CM');
+        $this->db->join(self::inventario_sucursal.' AS IS',' on IS.codigo_meterial = '.'CM.codigo_material');      
+        $this->db->join(self::unidad_medida.' AS UM',' on UM.id_unidad_medida = '.'CM.id_unidad_medida');
+        $this->db->where('CM.codigo_material',$codigo_material);        
+        $query = $this->db->get();
+        
+        if($query->num_rows() > 0 )
+        {
+            return $query->result();
+        }
+    }
+
+    // INSERTAR PEDIDO - ENCABEZADO
+    public function InsertPedido($Mesa,$Id_Mesero,$Id_Sucursal){
+        session_start();
+        $date = date("Y-m-d H:m:s");
+        $data = array(
+            'id_sucursal'       => $Id_Sucursal,           
+            'id_usuario'        => $_SESSION['idUser'],
+            'id_mesero'         => $Id_Mesero, 
+            'numero_mesa'       => $Mesa,            
+            'flag_cancelado'    => 0,
+            'fechahora_pedido'  => $date,
+            'flag_elaborado'    => 0,
+            'flag_despachado'   => 0,
+            'flag_pausa'        => 0,
+            'estado'            => 1,
+        );
+        $this->db->insert(self::sys_pedido,$data);
+        return $this->db->insert_id();
+    }
+
+    // INSERTAR PEDIDO - DETALLE
+    public function InsertPedidoDetalle($Mesa,$Id_Mesero,$Id_Producto,$Precio,$Id_Sucursal,$Id_Pedido){
+        session_start();
+        $date = date("Y-m-d H:m:s");
+        $data = array(
+            'id_pedido'         => $Id_Pedido,           
+            'id_producto'       => $Id_Producto,
+            'nodo'              => "",            
+            'precio_grabado'    => $Precio,
+            'precio_original'   => $Precio,
+            'estado'            => 0,
+        );
+        $this->db->insert(self::sys_pedido_detalle,$data);
+        return $this->db->insert_id();
+    }
     
+    // INSERTAR PEDIDO DETALLE MATERIA 
+    public function setPedidoDetalleMateria($id_pedido_detalle,$unidad_medida_id,$nombre_producto,$name_detalle,$cantidad){
+        session_start();
+        $date = date("Y-m-d H:m:s");
+        $data = array(
+            'id_detalle'        => $id_pedido_detalle,           
+            'id_unidad'         => $unidad_medida_id,
+            'nombre_producto'   => $nombre_producto, 
+            'codigo_producto'   => $name_detalle,            
+            'cantidad'          => $cantidad,
+            'total'             => $cantidad
+        );
+        $this->db->insert(self::sys_pedido_detalle_materia,$data);
+        return $this->db->insert_id();
+    }    
 }
 /*
  * end of application/models/consultas_model.php
