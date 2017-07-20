fc2c5e24a26fb4c1653655588e7829afb95a664c
rafael.gutierrez <rafael.gutierrez@telusinternationalcom>
2017-07-19 19:43:50 -0600

    data

--- application/views/backend/sucursales/VNodo.php
+++ application/views/backend/sucursales/VNodo.php
@@ -14,7 +14,15 @@
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
 	<!-- <script src="http://45.33.3.227/lapizzeria/js/longpoll.js"></script> -->
+
+	<script type="text/javascript">
+
+
+	</script>
+
+
 	<script src="/lapizzeria/js/longpoll.js"></script>
+
 	<script>
 		//var requestUrl = "http://45.33.3.227/lapizzeria/demo.php";
 		var requestUrl = "/lapizzeria/demo.php";
@@ -24,17 +32,79 @@
 		var data 		= {"id_sucursal":id_sucursal, "id_nodo":id_nodo};
 		var html 		= "";
 
-
-
 		/*
 		/ =================================================
 		*/
-		var callBack = function(response){
+		var response;
+		function init(){
+				$.ajax({
+		            url: "/lapizzeria/demo2.php",
+		            type:"post",
+		            data:data,
+		            success: function(data){     
+		            	response = JSON.parse(data);	
+						//console.log(response);
+				
+						if(response.success == 0)
+						{
+							var contador=1;
+							console.log(response);
+							for(var i=0 ; i<response.pedido.length ; i++)
+							{
+								html += "<div class='wrapper' id='"+response.pedido[i]['numero_mesa']+"' secuencia='"+response.pedido[i]['secuencia_orden']+"' pedido='"+response.pedido[i]['id_pedido']+"'><div class='list-group abc'><a href='#' class='list-group-item active'><i class='fa fa-home'></i>ORDEN -  # "+response.pedido[i]['secuencia_orden']+"</a>";
+							
+								// pedido
+								var ID_PEDIDO_VALUE = response.pedido[i]['id_pedido'];
+								html += "<a href='#' name='' class='list-group-item nodo'><table class='table table-hover'>";
+								html += "<tr><td>#</td><td>Producto</td></tr>";
+								
+								for(var j=0 ; j< response.detalle.length; j++)
+								{
+									// Detalle Productos
+									html += "<tr><td>"+contador+"</td>";
+									html += "<td><img src='../../../../../../assets/images/icon-no-elaborado.png' width='20px'/>"+response.detalle[j]['nombre_producto'];
+										if(response.detalle[i].items){
+											for(var x=0; x < response.detalle[i].items.length; x++){	
+												if(response.detalle[i].items[x])
+												{
+													html += "<ul>";
+													if(response.detalle[j].items[x]['eliminado']==1)								
+													{
+														html += "<li> Quitar -> "+response.detalle[j].items[x]['nombre_matarial']+"</li>";
+													}
+													if(response.detalle[j].items[x]['adicional']==1)
+													{
+														html += "<li> Agregar -> "+response.detalle[j].items[x]['nombre_matarial']+"</li>";	
+													}
+													html += "</ul>";																				
+												}
+											}
+										}
+											html +="</td></tr>";
+									contador++;
+								}					
+							}
+							html += "</table></a>";
+							html += "</div><span class='tiempo'></div>";
+							$('.ordenes').append(html);		
+							html="";
+						}
+
+					},
+					error:function(){
+					    //alert("Error.. No se subio la imagen");
+					}
+				}); 
+		}
+		init();
+
+		var callBack = function(response)
+		{
 			response = JSON.parse(response);	
 			//console.log(response);
 				
-			if(response.success == 0){
-
+			if(response.success == 0)
+			{
 				var contador=1;
 				console.log(response);
 				for(var i=0 ; i<response.pedido.length ; i++)
@@ -78,11 +148,12 @@
 				html="";
 			}
 
-				var tiempo = {
-	        		hora: 0,
-	        		minuto: 0,
-	        		segundo: 0
-    			};
+			var tiempo = 
+			{
+	        	hora: 0,
+	        	minuto: 0,
+	        	segundo: 0
+    		};
 
     		var tiempo_corriendo = null;
 
@@ -105,45 +176,39 @@
 		                $(this).find(".hora").text(tiempo.hora < 10 ? '0' + tiempo.hora : tiempo.hora);
 		                $(this).find(".minuto").text(tiempo.minuto < 10 ? '0' + tiempo.minuto : tiempo.minuto);
 		                $(this).find(".segundo").text(tiempo.segundo < 10 ? '0' + tiempo.segundo : tiempo.segundo);
-		            }, 1000);
-			
+		    }, 1000);			
 		}
 
 		longpoll(requestUrl, data, callBack);
-
 	</script>
 
 	<script>
+		$(function(){
+			$(document).on('click', 'div.wrapper', function(){
 
-	$(function(){
-		$(document).on('click', 'div.wrapper', function(){
+				$(this).css("background","green");
 
-			$(this).css("background","green");
+				var ID_mesa = $(this).attr('id');
+				var ID_pedido = $(this).attr('pedido');
+				var iSecuencia = $(this).attr('secuencia');
+				if (confirm('Despachar Orden # '+ iSecuencia +" Mesa :"+ ID_mesa + '?'))
+		        {	            
+		            $.ajax({
+					    url: "../../despacharPedido/"+ID_pedido+"/"+id_sucursal+"/"+id_nodo,
+					    type:"post", 
 
-			var ID_mesa = $(this).attr('id');
-			var ID_pedido = $(this).attr('pedido');
-			var iSecuencia = $(this).attr('secuencia');
-			if (confirm('Despachar Orden # '+ iSecuencia +" Mesa :"+ ID_mesa + '?'))
-	        {	            
-	            $.ajax({
-				    url: "../../despacharPedido/"+ID_pedido+"/"+id_sucursal+"/"+id_nodo,
-				    type:"post", 
-
-				    success: function(){     
-				    	
-				    },
-				    error:function(){            
-				        alert("Error Al Despachar Pedido");
-				    }
-				}); 
-				$(this).remove();
-	            return;
-	        }        	
-        	
-        		
-        	
-        });
-	});
+					    success: function(){     
+					    	
+					    },
+					    error:function(){            
+					        alert("Error Al Despachar Pedido");
+					    }
+					}); 
+					$(this).remove();
+		            return;
+		        }        	
+	        });
+		});
 	</script>
 </head>
 <style>
